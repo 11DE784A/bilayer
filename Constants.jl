b611f28bfe35f9a9c48201f7bdbb1918014df7ee
Ayush Singh <ayush.singh@niser.ac.in>
2020-12-22 22:44:44 +0530

    fresh approach to twisted bilayer

--- Constants.jl
+++ Constants.jl
@@ -11,13 +11,34 @@ const m_e = 9.109e-31u"kg"
 
 # graphene constants
 const a = 2.46e-10u"m"               # lattice constant
-λ(j) = ([3.16, 0.381]u"eV")[j+1]     # λ(0), λ(1) parametrize in-plane, dimer hoppings
+λ(j) = ([3.16, 0.381, 0.0, 
+         0.38, 0.14]u"eV")[j+1]      # λ(0), λ(1) parametrize in-plane, dimer hoppings
 v(j) = (√3a*λ(j)) / 2ħ               # v(0), v(1) fermi velocities
-const m = λ(1) / (2v(0)^2)           # effective mass of quasiparticles
+const m_eff = λ(1) / (2v(0)^2)           # effective mass of quasiparticles
 const γ = 0.9μ_B                     # Zeeman coupling
 const γ̄  = 1.7μ_B                    # pseudo-Zeeman coupling
 const A = (1e3a)^2                   # area of sample
 
 # some helper functions
+δ(i, j) = i == j ? 1 : 0             # Kronecker delta
 l(B) = sqrt(ħ/(e*abs(B)))            # Landau radius
 β(T) = (k_B*T)^-1                    # thermodynamic β
+
+# Pauli matrices
+σ(j) = [δ(j, 3)     δ(j, 1) - im*δ(j, 2);
+        δ(j, 1) + im*δ(j, 2)    -δ(j, 3)]
+
+# for twisted bilayer graphene
+const w = 0.11eV                    # interlayer hopping parameter
+
+k(θ) = (8π/3a)*sin(θ/2)
+
+q_b(θ) = k(θ)*[0, -1]
+q_tr(θ) = k(θ)*[√3/2,  1/2]
+q_tl(θ) = k(θ)*[-√3/2, 1/2]
+
+ϕ = 2π/3
+T_b = [1 1; 1 1]
+T_tr = [exp(-im*ϕ) 1; exp(im*ϕ) exp(-im*ϕ)]
+T_tl = [exp(im*ϕ) 1;  exp(-im*ϕ) exp(im*ϕ)]
+O = zeros(2, 2)
---
+++ Spectra.jl
@@ -0,0 +1,68 @@
+using Printf
+using Unitful, UnitfulRecipes
+using Plots
+using Roots
+using LinearAlgebra
+using LaTeXStrings
+
+include("PlotSettings.jl")
+include("Constants.jl")
+
+const dim = 100
+
+const m_dot = 0.067m_e
+const l_dot = 50u"nm"
+const ω_d = ħ / (m_dot*l_dot^2)
+
+ω_dot(B) = e*B / m_dot
+Ω_dot(B) = sqrt(ω_d^2 + ω_dot(B)^2/4)
+
+Ω(ξ, B, B_S) = v(0) * sqrt(2*e*abs(B + ξ*B_S)/ħ)
+Δ(ξ, B, B_S) = (γ̄*B_S) / (ħ*Ω(ξ, B, B_S))
+N(ξ, B, B_S) = floor((abs(B + ξ*B_S) * A) / (h/e))
+
+plottable(sp) = transpose(hcat(sp...))
+
+spectrum(E, B) = reshape([E(n, s, ξ, B) 
+                          for n in 0:dim, 
+                              s in [+1, -1], 
+                              ξ in [+1, -1]],
+                         :, 1)
+
+# semiconductor quantum dot
+E_dot(n, m, B) = ħ*Ω_dot(B)*(2n + abs(m) +1) - m*ħ*ω_dot(B)/2
+sp_dot(B) = (rdim = floor(sqrt(dim)); 
+             reshape([E_dot(n, m, B) for n in 0:rdim, m in -rdim:rdim], :, 1))
+
+# monolayer graphene
+E_mono(n, s, ξ, B) = (sqrt(2n)*ħ*v(0) / l(B)) - s*γ*B
+sp_mono(B) = spectrum(E_mono, B)
+
+# strained monolayer graphene
+E_strm(n, s, ξ, B, B_S) = ((n == 0 ?  ξ*ħ*Ω(ξ,B,B_S)*Δ(ξ,B,B_S) : 
+                                      ħ*Ω(ξ,B,B_S)*sqrt(n+Δ(ξ,B,B_S)^2)) - s*γ*B)
+sp_strm(B, B_S) = spectrum((n, s, ξ, B) -> E_strm(n, s, ξ, B, B_S), B)
+
+# bilayer graphene
+E_bi(n, s, ξ, B) = ħ*(e*B/m_eff) * sqrt(n*(n-1)) - s*γ*B
+sp_bi(B) = spectrum(E_bi, B)
+
+# twisted bilayer graphene
+h_m(k) = -ħ*v(0)*[0u"m^-1"  k[1] + im*k[2];
+                  k[1] - im*k[2]  0u"m^-1"]
+
+function sp_twist(θ, B)
+    Π = Bidiagonal(zeros(dim+1), sqrt.(1:dim), :U)
+    U = Bidiagonal(zeros(8), [1., 0, 1, 0, 1, 0, 1], :U)
+    L = U'
+
+    H_c = ustrip.(eV, [O*eV     w*T_b        w*T_tr        w*T_tl;
+                       w*T_b'   h_m(q_b(θ))  O*eV          O*eV;
+                       w*T_tr'  O*eV         h_m(q_tr(θ))  O*eV;
+                       w*T_tl'  O*eV         O*eV  h_m(q_tl(θ))])
+
+    H_k = (√2*ħ*v(0)/l(B)) * (kron(U, Π') + kron(L, Π))
+
+    H_b = ustrip.(eV, H_k) + kron(H_c, I(dim+1))
+    return eigvals(H_b)*eV
+end
